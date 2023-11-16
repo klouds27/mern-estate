b3fb8c7c176c530f884eda1460fcca6f6530c4fd
Klouds27 <adalwolfad@gmail.com>
2023-11-16 13:49:41 +0100

    updated controllers and components

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,12 +1,14 @@
 import express from 'express';
+import { deleteUser, test, updateUser,  getUserListings, getUser} from '../controllers/user.controller.js';
+import { verifyToken } from '../utils/verifyUser.js';
+
 
 const router = express.Router();
 
-router.get('/', (req, res) => { 
-    res.json({ 
-        message: 'Hello World' });
-
-
-});
+router.get('/test', test);
+router.post('/update/:id', verifyToken, updateUser)
+router.delete('/delete/:id', verifyToken, deleteUser)
+router.get('/listings/:id', verifyToken, getUserListings)
+router.get('/:id', verifyToken, getUser)
 
 export default router;
