229089070153a81513dbdc41d04d2bb14dac41b9
Klouds27 <adalwolfad@gmail.com>
2023-11-18 17:41:41 +0100

    update path on index.js

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 import path from 'path';
-dotenv.config({ path: path.resolve(__dirname, './.env') });
+dotenv.config();
 
 mongoose
   .connect(process.env.MONGO)
